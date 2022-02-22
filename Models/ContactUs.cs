53b832a1d2f1b0a94694ad444a3b426c32ee2cfd
0verwritten <jadovirlyvyjlox@gmail.com>
2022-02-22 17:08:45 +0200

    adding another model to project

---
+++ Models/ContactUs.cs
@@ -0,0 +1,29 @@
+// using MongoDB.Bson;
+// using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace University_Lander.Models
+{
+    public class ContactUs
+    {
+        // [BsonId]
+        // [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public List<string> Email { get; set; }
+        public List<SocialLink> SocialNets { get; set; }
+        public List<string> PhoneNumber { get; set; }
+
+        public LocationAddress Location;
+    }
+
+    public class SocialLink{
+        public string Name;
+        public Uri LinkAddress;
+        public Uri Icon;
+    }
+
+    public class LocationAddress{
+        public string Location;
+        public string Coordinates;
+    }
+}
